a263c2591882245dc99277fc6de7402f14f2c5b4
Sami <sami@MacBook-Pro.hitronhub.home>
2021-03-01 15:44:44 -0800

    added ec2 user data with the hostname

---
+++ ec2-user-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Use this for youruser data (script without newlines)
+# install httpd (linux 2 version)
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
